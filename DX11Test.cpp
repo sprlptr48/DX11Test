6c24050cb6893a97b3129717c55b291a4fefb687
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2024-03-26 23:42:27 +0300

    https://graphicsprogramming.github.io/learnd3d11/1-introduction/1-1-getting-started/1-1-1-hello-window/#hellowindowapplicationcpp

--- DX11Test.cpp
+++ DX11Test.cpp
@@ -1,20 +1,48 @@
-// DX11Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <GLFW/glfw3.h>
+#include <cstdint>
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "Hello World!\n";
+    if (!glfwInit())
+    {
+        std::cout << "GLFW: Unable to initialize\n";
+        return -1;
+    }
+
+    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* videoMode = glfwGetVideoMode(primaryMonitor);
+    const int32_t width = static_cast<int32_t>(videoMode->width * 0.8f);
+    const int32_t height = static_cast<int32_t>(videoMode->height * 0.8f);
+
+    glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_FALSE);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(
+        width,
+        height,
+        "LearnD3D11 - Hello Window",
+        nullptr,
+        nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "GLFW: Unable to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    const int32_t windowLeft = videoMode->width / 2 - width / 2;
+    const int32_t windowTop = videoMode->height / 2 - height / 2;
+    glfwSetWindowPos(window, windowLeft, windowTop);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        // future update code
+        // future render code
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
