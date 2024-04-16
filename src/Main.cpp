f42e6d9cf11592ff9aaa96d22bb4637036ec084a
sprlptr48 <63254295+sprlptr48@users.noreply.github.com>
2024-04-16 20:23:48 +0300

    Move to CMake because why not

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,10 @@
 #include "HelloWindowApp.hpp"
-#include "HelloD3D11.hpp"
+#include "HiTriangle.hpp"
+
 
 int main(int argc, char* argv[])
 {
-    HelloD3D11 dxwindow{ "DX11 Window!" };
+    HiTriangle dxwindow{ "DX11 Window!" };
     dxwindow.Run();
     return 0;
 }
